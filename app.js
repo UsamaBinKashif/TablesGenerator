1dd9a8001df3fe1f07b920f1af6a94eea18b4351
Usama Ahmed <usamabinkashif.98@gmail.com>
2021-10-05 04:12:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let table = +prompt('Enter which table you want? \n For exmaple: 6')
+
+    document.getElementById('td1').innerHTML = (table + " " + "x" + " " + 1 + " " + " = " + " " + table * 1);
+    document.getElementById('td2').innerHTML = (table + " " + "x" + " " + 2 + " " + " = " + " " + table * 2);
+    document.getElementById('td3').innerHTML = (table + " " + "x" + " " + 3 + " " + " = " + " " + table * 3);
+    document.getElementById('td4').innerHTML = (table + " " + "x" + " " + 4 + " " + " = " + " " + table * 4);
+    document.getElementById('td5').innerHTML = (table + " " + "x" + " " + 5 + " " + " = " + " " + table * 5);
+    document.getElementById('td6').innerHTML = (table + " " + "x" + " " + 6 + " " + " = " + " " + table * 6);
+    document.getElementById('td7').innerHTML = (table + " " + "x" + " " + 7 + " " + " = " + " " + table * 7);
+    document.getElementById('td8').innerHTML = (table + " " + "x" + " " + 8 + " " + " = " + " " + table * 8);
+    document.getElementById('td9').innerHTML = (table + " " + "x" + " " + 9 + " " + " = " + " " + table * 9);
+    document.getElementById('td10').innerHTML = (table + " " + "x" + " " + 10 + " " + " = " + " " + table * 10);
